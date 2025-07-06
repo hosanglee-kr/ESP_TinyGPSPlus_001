2c0e73919ea736d1a9ca732a2f533a70a539ab98
hslee <50106977+hosanglee-kr@users.noreply.github.com>
2025-07-07 08:59:30 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,24 @@
 #include <Arduino.h>
 
-// The build version comes from an environment variable. Use the VERSION
-// define wherever the version is needed.
-#define STRINGIZER(arg) #arg
-#define STR_VALUE(arg)	STRINGIZER(arg)
-#define VERSION			STR_VALUE(BUILD_VERSION)
-
-#define S10
-#ifdef S10
-	#include "S10_streams-i2s-webserver_wav_001.h"
+#define T10
+#ifdef T10
+	#include "T10_GPS_001.h"
 #endif
 
 void setup() {
 	// delay(5000);
-
 	Serial.begin(115200);
-	Serial.print("Version: ");
-	Serial.println(VERSION);
-
-#ifdef S10
-	S10_setup();
-#endif
+	
+    #ifdef T10
+	    T10_initGpsModule();
+    #endif
 
 	Serial.println("11111");
 }
 
 void loop() {
-#ifdef S10
-	S10_loop();
-#endif
+    #ifdef T10
+	    T10_processGpsData();       // GPS 데이터 파싱 함수 호출
+        T10_handleGpsUpdateAndPrint(); 
+    #endif
 }
